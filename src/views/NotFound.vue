d08605ebe292f3a8e02adb8388ffc0fda8dee0d5
Nafas <nafiseh.ameri@partsoftware.com>
2024-01-16 17:38:27 +0330

    service, not found

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -7,11 +7,10 @@
     <div class="container text-center py-5">
       <div class="row justify-content-center">
         <div class="col-lg-6">
-          <h1 class="display-1">خطای 404</h1>
+          <h1 class="display-1 ">خطای 404</h1>
           <h1>صفحه پیدا نشد</h1>
           <p class="mb-4">
-            متأسفیم، صفحه ای که به دنبال آن بودید در وب سایت ما وجود ندارد! شاید
-            به صفحه اصلی ما بروید یا سعی کنید از جستجو استفاده کنید؟
+            متأسفیم، صفحه ای که به دنبال آن بودید در وب سایت ما وجود ندارد!
           </p>
           <a class="btn btn-primary py-3 px-5" href="">برگرد به خانه</a>
         </div>
@@ -23,4 +22,4 @@
 <script setup></script>
 
 <style lang="css" scoped src="@/assets/css/bootstrap.v5.0.0.min.css"></style>
-<style lang="scss" scoped src="@/assets/scss/consultancy.scss"></style>
+<style lang="scss" scoped src="@/assets/scss/notfound.scss"></style>
--- src/views/ServicePage.vue
+++ src/views/ServicePage.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="service" v-for="service in services" :key="service">
-    <div v-for="(item, index) in service.items" :key="index">
+  <div class="services" v-for="service in services" :key="service">
+    <div class="service" v-for="(item, index) in service.items" :key="index">
       <div class="service__img" v-if="this.$route.params.name == item.title">
         <img :src="require(`../assets/img/${item.img}`)" :alt="item.title" />
       </div>
-      <div class="service__title" v-if="this.$route.params.name == item.title">
-        {{ item.title }}
-      </div>
-      <div class="service__desc" v-if="this.$route.params.name == item.title">
-        {{ item.desc }}
+      <div
+        class="service__content"
+        v-if="this.$route.params.name == item.title"
+      >
+        <div class="service__title">{{ item.title }}</div>
+        <div class="service__desc">{{ item.desc }}</div>
       </div>
     </div>
   </div>
@@ -17,15 +18,9 @@
 <script>
 import { mapState } from "vuex";
 export default {
-  //   props: {
-  // serviceData: Object,
-  //   },
   computed: {
     ...mapState(["services"]),
   },
-  //   onMounted(() => {
-  //     this.$service = "";
-  //   }),
 };
 </script>
 
