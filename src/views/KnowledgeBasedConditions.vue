eb4321adda1afe99262407110c405ffc525a8666
fatemeh <fatemeh.hasanzadeh97@yahoo.com>
2024-10-12 15:45:14 +0330

    responsive the KnowledgeBaseCompany page

--- src/views/KnowledgeBasedConditions.vue
+++ src/views/KnowledgeBasedConditions.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container dir-rtl ">
     <h1 class="title-color">شرایط دانش بنیان شدن و ثبت شرکت دانش بنیان</h1>
-    <p class="text-white">
+    <p class="text-white justify-text ">
       به طور کلی "دانش بنیان" به محصول یا خدمت تعلق می‌گیرد و برای اینکه
       محصول یا خدمت خود را دانش بنیان کنید، حتماً باید شرکت داشته باشید و
       درواقع شخصیت حقوقی باشید.<br />
@@ -16,11 +16,11 @@
 
 <div class="container dir-rtl">
     <h1 class="title-color">معیارهای ارزیابی شرکت‌ها، کالاها و خدمات دانش بنیان:</h1>
-    <p class="text-white">
+    <p class="text-white justify-text ">
       شرکت‌های متقاضی باید به نکات زیر توجه نمایند و در نظر داشته باشند که محصولات و کالاهای
       عرضه شده، می‌بایست تمامی این خصوصیات و شرایط را در بر داشته باشد:
     </p>
-    <ul class="custom-list">
+    <ul class="custom-list justify-text ">
       <li>
         <div class="highlight">شرط تولید:</div> 
         محصول ارائه شده می‌بایست در فرایند تولید باشد و یا حداقل در حد نمونه آزمایشگاهی ساخته شده باشد.<br>
@@ -72,5 +72,32 @@ export default {
   color: #f5841a; 
   font-weight: bold; 
 }
+.justify-text {
+  text-align: justify; /* تراز کردن متن به صورت یکنواخت */
+}
 
+/* Media Queries برای تغییر فونت‌ها و سایز‌ها در نمایشگرهای مختلف */
+@media (max-width: 768px) {
+  .container-fluid {
+    padding: 15px;
+  }
+  h1 {
+    font-size: 1.5rem;
+  }
+  p, .custom-list li {
+    font-size: 0.9rem;
+  }
+}
+
+@media (max-width: 576px) {
+  .container-fluid {
+    padding: 10px;
+  }
+  h1 {
+    font-size: 1.2rem;
+  }
+  p, .custom-list li {
+    font-size: 0.8rem;
+  }
+}
 </style>
