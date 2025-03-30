c626b90a7b2ecf85864cc539045610e34ff94fd4
Fatemehsnz <fatemeh.hasanzadeh97@yahoo.com>
2025-03-30 14:38:23 +0330

    try to making WhyChooseUs responsive

--- src/views/WhyChooseUs.vue
+++ src/views/WhyChooseUs.vue
@@ -1,46 +1,52 @@
 <template>
-  <div class="container-fluid py-6 px-5">
+  <div class="container-fluid py-5 px-5">
     <div class="text-center mx-auto mb-5" style="max-width: 600px">
-      <h1 class="display-5 mb-0">چرا ما!!!</h1>
+      <h1 class="display-5 mb-0" style="color: rgb(242, 232, 198)">چرا ما!!!</h1>
       <hr class="w-25 mx-auto bg-primary" />
     </div>
-    <div class="row g-5 dir-rtl">
-      <div class="col-lg-4">
-        <div class="row g-5">
-          <div class="col-12">
-            <div class="triangle mb-3">
-              <i class="fa fa-cubes fs-4 text-white"></i>
-            </div>
-            <h3>چرا مشاور؟</h3>
-            <p class="mb-0">
-              ارائه خدماتی که موجب رشد سریع تر سازمان ها، کاهش دغدغه های
-              مدیریتی، کاهش هزینه‌های نیروی انسانی و نیل به نتیجه مطلوب در کوتاه
-              ترین زمان ممکن شود. ما با ایجاد روابط مبتنی بر اعتماد و با تکیه به
-              دانش و تخصص خود، شما را در مسیر موفقیت همراهی خواهیم نمود.
-            </p>
+    <div class="container dir-rtl my-5">
+      <div class="row ">
+        <!-- ستون اول -->
+        <div class="col-md-4">
+          <div class="triangle mb-3">
+            <i class="fa fa-cubes fs-4 text-white"></i>
           </div>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="d-block bg-primary h-100 text-center">
-          <img class="img-fluid" src="@/assets/img/chess.jpg" alt="" />
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="row g-5">
-          <div class="col-12">
-            <div class="triangle mb-3">
-              <i class="far fa-smile-beam fs-4 text-white"></i>
-            </div>
-            <h3>چرا آشا؟</h3>
-            <p class="mb-0">
-              ایجاد بازوی کمکی و پیشران برای شرکت های تولیدی در فرآیند کسب
-              مجوزها و گواهینامه های مورد نیاز، به نحوی که با دانش متخصصان خود
-              در این حوزه، همانند یک شتاب دهنده عمل کرده و در کوتاه ترین مسیر،
-              با رعایت الزامات و استانداردها، آن را محقق نموده و تمرکز آن مجموعه
-              را در تولید محصولی با کیفیت حفظ نماید.
+          <h3>چرا مشاور؟</h3>
+          <h5 style="color: rgb(242, 232, 198)">
+            هر سازمان، برای رشد مستمر،<br />
+            همواره نیاز به نگاه از بیرون دارد:
+          </h5>
+          <p class="mb-0 text-justify">
+             همانند یک بازی شطرنج، کسی که تماشا می کند، معمولا پیشنهاد حرکت های بهتری دارد. نگاه از بیرون به اجزای سازمان، بی طرفانه و منطقی تر بوده و تصمیمات واقع گرایانه خواهد بود. عموما انجام فعالیت های برون سپاری و پروژه ای، از استخدام نیروی ثابت و همیشگی، کم هزینه تر می باشد. تمرکز مشاور تنها روی پروژه همکاری بوده و درگیر سایر مسائل سازمان نیست، لذا سرعت پیشرفت بالا می باشد
+          </p>
+          <br />
+          <h5 style="color: rgb(242, 232, 198)">شبکه سازی و ارتباطات</h5>
+          <p class="mb-0 text-justify">
+          مشاور می تواند پلی باشد برای ارتباط با سایر صنایع و شبکه ایجاد شده منجر به خلق موقعیت های جدید در کسب و کار گردد. مشاور عموما دیدگاهی باز از مشکلات مشابه در صنایع مختلف دارد و می تواند راه حل های مناسبتری ارائه دهد.
             </p>
+        </div>
+
+        <!-- ستون دوم (تصویر) -->
+        <div class="col-md-4 ">
+          <img src="@/assets/img/chess.jpg" class="img-fluid" style="max-width: 100%; height: auto;" alt="مشاوره">
+        </div>
+
+        <!-- ستون سوم -->
+        <div class="col-md-4">
+          <div class="triangle mb-3">
+            <i class="far fa-smile-beam fs-4 text-white"></i>
           </div>
+          <h3>چرا آشا؟</h3>
+          <h5 style="color: rgb(242, 232, 198)">
+            گروه مشاوره آشا صرفا یک گروه دریافت مجوز نیست!
+          </h5>
+          <p class="mb-0 text-justify">
+          بلکه اعضای اصلی آن، دارای تجربه تولیدی و دانش فنی لازم در زمینه تولید تجهیزات پزشکی بوده و با چالش های موجود در تولید آشنا می باشند. لذا، در انجام یک پروژه، چه دریافت مجوز تولید، چه پیاده سازی سیستم های مدیریت کیفیت، دانش بنیان و ...، رویکرد ما تولید محور، کاهش دغدغه های مدیریتی، بهینه سازی و کارآیی فرآیندهای سازمان می باشد.
+          </p>
+          <br />
+          <p class="mb-0 text-justify">
+          بواسطه پتانسیل موجود در اعضای گروه و تجارب آنها، هنگام ورود به یک سازمان، مجموعه ای از خدمات فنی و مهندسی قابل ارائه می باشد، به همین دلیل، افتخار می نماییم که با شرکت هایی که همکاری داشته ایم، عموما ارتباط ما قطع نشده وهمواره در پروژه ها و اشکال مختلف در کنار آنها بوده‌ایم.
+          </p>
         </div>
       </div>
     </div>
@@ -64,6 +70,7 @@ export default {};
   border-left: 30px solid transparent; /* نیمه راست مثلث */
   border-right: 30px solid transparent; /* نیمه چپ مثلث */
   border-bottom: 60px solid #f5841a;
-  position: relative;
+
+  display: inline-block
 }
 </style>
---
+++ src/views/WhyChooseUsCopy.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container-fluid py-5 px-5">
+    <div class="text-center mx-auto mb-5" style="max-width: 600px">
+      <h1 class="display-5 mb-0" style="color: rgb(242, 232, 198)">چرا ما!!!</h1>
+      <hr class="w-25 mx-auto bg-primary" />
+    </div>
+    <div class="row g-5 dir-rtl">
+      <div class="col-lg-4 px-4">
+        <div class="row g-5">
+          <div class="col-12">
+            <div class="triangle mb-3">
+              <i class="fa fa-cubes fs-4 text-white"></i>
+            </div>
+            <h3>چرا مشاور؟</h3>
+            <h5 style="color: rgb(242, 232, 198)"> هر سازمان، برای رشد مستمر،<br> همواره نیاز به نگاه از بیرون دارد:</h5>
+            <p class="mb-0 text-justify">
+             همانند یک بازی شطرنج، کسی که تماشا می کند، معمولا پیشنهاد حرکت های بهتری دارد. نگاه از بیرون به اجزای سازمان، بی طرفانه و منطقی تر بوده و تصمیمات واقع گرایانه خواهد بود. عموما انجام فعالیت های برون سپاری و پروژه ای، از استخدام نیروی ثابت و همیشگی، کم هزینه تر می باشد. تمرکز مشاور تنها روی پروژه همکاری بوده و درگیر سایر مسائل سازمان نیست، لذا سرعت پیشرفت بالا می باشد
+            </p>
+            <br />
+            <h5 style="color: rgb(242, 232, 198)">شبکه سازی و ارتباطات</h5>
+            <p class="mb-0 text-justify">
+            مشاور می تواند پلی باشد برای ارتباط با سایر صنایع و شبکه ایجاد شده منجر به خلق موقعیت های جدید در کسب و کار گردد. مشاور عموما دیدگاهی باز از مشکلات مشابه در صنایع مختلف دارد و می تواند راه حل های مناسبتری ارائه دهد.
+             </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="d-block bg-primary text-center">
+          <img class="img-fluid" src="@/assets/img/chess.jpg" alt="chess" />
+        </div>
+      </div>
+      <div class="col-lg-4 px-4">
+        <div class="row g-5">
+          <div class="col-12">
+            <div class="triangle mb-3">
+              <i class="far fa-smile-beam fs-4 text-white"></i>
+            </div>
+            <h3>چرا آشا؟</h3>
+            <h5 style="color: rgb(242, 232, 198)">گروه مشاوره آشا صرفا یک گروه دریافت مجوز نیست!</h5>
+            <p class="mb-0 text-justify">
+            بلکه اعضای اصلی آن، دارای تجربه تولیدی و دانش فنی لازم در زمینه تولید تجهیزات پزشکی بوده و با چالش های موجود در تولید آشنا می باشند. لذا، در انجام یک پروژه، چه دریافت مجوز تولید، چه پیاده سازی سیستم های مدیریت کیفیت، دانش بنیان و ...، رویکرد ما تولید محور، کاهش دغدغه های مدیریتی، بهینه سازی و کارآیی فرآیندهای سازمان می باشد.
+            </p>
+            <br />
+            <p class="mb-0 text-justify">
+            بواسطه پتانسیل موجود در اعضای گروه و تجارب آنها، هنگام ورود به یک سازمان، مجموعه ای از خدمات فنی و مهندسی قابل ارائه می باشد، به همین دلیل، افتخار می نماییم که با شرکت هایی که همکاری داشته ایم، عموما ارتباط ما قطع نشده وهمواره در پروژه ها و اشکال مختلف در کنار آنها بوده‌ایم.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style
+  lang="css"
+  scoped
+  src="@/assets/css/bootstrap.v4.5.3.basco.min.css"
+></style>
+<style lang="scss" scoped src="@/assets/scss/consultancy.scss"></style>
+<style lang="scss" scoped>
+.triangle {
+  width: 0;
+  height: 0;
+  border-left: 30px solid transparent; /* نیمه راست مثلث */
+  border-right: 30px solid transparent; /* نیمه چپ مثلث */
+  border-bottom: 60px solid #f5841a;
+  position: relative;
+}
+</style>
